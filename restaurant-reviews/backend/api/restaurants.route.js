d432eccb4beb2f6fdcb851f07b3abcf4f828cc9b
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-09 11:06:42 +0100

    Update files

--- restaurant-reviews/backend/api/restaurants.route.js
+++ restaurant-reviews/backend/api/restaurants.route.js
@@ -1,10 +1,10 @@
 import express from "express"
-import VehiclesCtrl from "./vehicles.controller.js"
-import VehiclesInfoCtrl from "./vehicles_info.controller.js"
+//import VehiclesCtrl from "./vehicles.controller.js"
+//import VehiclesInfoCtrl from "./vehicles_info.controller.js"
 
 const router = express.Router()
 
-router.route("/").get((req, res) => res.send("Hello World"))
+router.route("/api/v1/restaurants/").get((req, res) => res.send("Hello World"))
 
 /*
 router.route("/").get(VehiclesCtrl.apiGetVehicles)
