79ae5512a247a69485226ec0f9e02cadd6b1f6ed
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-09 20:50:07 +0100

    Update files

--- restaurant-reviews/backend/dao/restaurantsDAO.js
+++ restaurant-reviews/backend/dao/restaurantsDAO.js
@@ -6,7 +6,7 @@ export default class RestaurantsDAO {
             return
         }
         try {
-            restaurants = await conn.db(process.env.VIMAPP_NS).collection("restaurants")
+            restaurants = await conn.db(process.env.RESTREVIEWS_NS).collection("restaurants")
         } catch (e) {
             console.error(
                 `Unable to establish a collection handle in restaurantsDAO: ${e}`
