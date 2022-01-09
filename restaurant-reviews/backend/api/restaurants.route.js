097b081cea4b863879a014f1de55eecea0b22ab0
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-09 10:25:55 +0100

    Update changes

---
+++ restaurant-reviews/backend/api/restaurants.route.js
@@ -0,0 +1,15 @@
+import express from "express"
+import VehiclesCtrl from "./vehicles.controller.js"
+import VehiclesInfoCtrl from "./vehicles_info.controller.js"
+
+const router = express.Router()
+
+router.route("/").get(VehiclesCtrl.apiGetVehicles)
+
+router
+    .route("/vehicles_info")
+    .post(VehiclesInfoCtrl.apiPostVehicleInfo)
+    .put(VehiclesInfoCtrl.apiUpdateVehicleInfo)
+    .delete(VehiclesInfoCtrl.apiDeleteVehicleInfo)
+
+export default route
