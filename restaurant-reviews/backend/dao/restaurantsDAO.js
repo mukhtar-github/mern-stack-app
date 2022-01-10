a3de89d58df3915a7b8a10f920f63c8c8fbc98d1
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-10 11:45:53 +0100

    Update file

--- restaurant-reviews/backend/dao/restaurantsDAO.js
+++ restaurant-reviews/backend/dao/restaurantsDAO.js
@@ -21,37 +21,37 @@ export default class RestaurantsDAO {
     } = {}) {
         let query
         if (filters) {
-            if ("vehicle_id" in filters) {
-                query = { $text: { $search: filters["vehicle_id"] } }
-            } else if ("model" in filters) {
-                query = { "model": { $eq: filters["model"] } }
-            } else if ("attached_to" in filters) {
-                query = { "attached_to": { $eq: filters["attached_to"] } }
+            if ("name" in filters) {
+                query = { $text: { $search: filters["name"] } }
+            } else if ("cuisine" in filters) {
+                query = { "cuisine": { $eq: filters["cuisine"] } }
+            } else if ("zipcode" in filters) {
+                query = { "zipcode": { $eq: filters["zipcode"] } }
             }
         }
 
         let cursor
 
         try {
-            cursor = await vehicles
+            cursor = await restaurants
             .find(query)
         } catch (e) {
             console.error(`Unable to issue find command, ${e}`)
-            return { vehiclesList: [], totalNumVehicles: 0 }
+            return { restaurantsList: [], totalNumRestaurants: 0 }
         }
 
-        const displayCursor = cursor.limit(vehiclesPerPage).skip(vehiclesPerPage * page)
+        const displayCursor = cursor.limit(restaurantsPerPage).skip(restaurantsPerPage * page)
 
         try {
-            const vehiclesList = await displayCursor.toArray()
-            const totalNumVehicles = await vehicles.countDocuments(query)
+            const restaurantsList = await displayCursor.toArray()
+            const totalNumrestaurants = await restaurants.countDocuments(query)
 
-            return { vehiclesList, totalNumVehicles }
+            return { restaurantsList, totalNumRestaurants }
         } catch (e) {
             console.error(
                 `Unable to convert cursor to array or problem counting documents, ${e}`
             )
-            return { vehiclesList: [], totalNumVehicles: 0 }
+            return { restaurantsList: [], totalNumRestaurants: 0 }
         }
     }
 }
