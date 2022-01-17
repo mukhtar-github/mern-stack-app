b74a6c42cf53389a52df4071c72807eb6b608fda
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-17 16:44:08 +0100

    Update files

--- restaurant-reviews/frontend/src/App.js
+++ restaurant-reviews/frontend/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
