2a3a68bfaca7f15a811d0954bef7d6c1b11209d7
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-25 05:37:56 +0100

    Update file

--- restaurant-reviews/frontend/src/components/restaurants-list.js
+++ restaurant-reviews/frontend/src/components/restaurants-list.js
@@ -161,7 +161,7 @@ const RestaurantsList = props => {
                   <Link to={"/restaurants/"+restaurant._id} className="btn btn-primary col-lg-5 mx-1 mb-1">
                     View Reviews
                   </Link>
-                  <a target="_blank" href={"https://www.google.com/maps/place/" + address} className="btn btn-primary col-lg-5 mx-1 mb-1">View Map</a>
+                  <a target="_blank"  rel="noreferrer" href={"https://www.google.com/maps/place/" + address} className="btn btn-primary col-lg-5 mx-1 mb-1">View Map</a>
                   </div>
                 </div>
               </div>
