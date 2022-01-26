c966f66656beebb72c0e8aa03761091fc2ab0a32
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-26 06:45:51 +0100

    Update file

--- restaurant-reviews/frontend/src/components/restaurants-list.js
+++ restaurant-reviews/frontend/src/components/restaurants-list.js
@@ -88,7 +88,7 @@ const RestaurantsList = props => {
   return (
     <div>
       <div className="row pb-1">
-        <div className="input-group col-lg-4">
+        <div className="input-group col-lg">
           <input
             type="text"
             className="form-control"
@@ -106,7 +106,7 @@ const RestaurantsList = props => {
             </button>
           </div>
         </div>
-        <div className="input-group col-lg-4">
+        <div className="input-group col-lg">
           <input
             type="text"
             className="form-control"
@@ -124,7 +124,7 @@ const RestaurantsList = props => {
             </button>
           </div>
         </div>
-        <div className="input-group col-lg-4">
+        <div className="input-group col-lg">
 
           <select onChange={onChangeSearchCuisine}>
              {cuisines.map(cuisine => {
@@ -149,7 +149,7 @@ const RestaurantsList = props => {
         {restaurants.map((restaurant) => {
           const address = `${restaurant.address.building} ${restaurant.address.street}, ${restaurant.address.zipcode}`;
           return (
-            <div className="col-lg-4 pb-1">
+            <div className="col-lg pb-1">
               <div className="card">
                 <div className="card-body">
                   <h5 className="card-title">{restaurant.name}</h5>
