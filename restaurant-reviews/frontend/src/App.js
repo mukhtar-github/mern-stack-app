aff7bc32a7c4f1d043778b813ecd991a07aac81d
Mukhtar <mukhtartanimugarba@gmail.com>
2022-04-18 07:17:07 +0100

    Update file

--- restaurant-reviews/frontend/src/App.js
+++ restaurant-reviews/frontend/src/App.js
@@ -3,65 +3,13 @@ import { Switch, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-
-import AddReview from "./components/add-review";
-import Restaurant from "./components/restaurants";
-import RestaurantsList from "./components/restaurants-list";
-import Login from "./components/login";
-
 function App() {
-  const isAuthenticated = Auth.isAuthenticated();
-
-    login = () => {
-      setState({ isAuthenticated: true });
-    }
-
-    logout = () => {
-      setState({ isAuthenticated: false });
-    }
+  
   return (
-    <div>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="/restaurants">Restaurant Reviews</Navbar.Brand>
-          <Nav className="me-auto">
-          <Nav.Link href="/restaurants">Restaurants</Nav.Link>
-          <Nav.Link href="/login">Login</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      
-
-      <Routes>
-          <Route path={["/", "/restaurants"]} element={<RestaurantsList /> } />
-          <Route path="/restaurants/:id/review" element = { <AddReview user={user} /> } />
-          <Route path="/restaurants/:id" element={ <Restaurant user={user} /> } />
-          <Route path="/login" element = { <Login login={login} /> } />
-        </Routes>
+    <div className='App'>
+      Hello World!
     </div>
-  );
+  )
 }
+
 export default App;
-<nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/restaurants" className="navbar-brand">
-          Restaurant Reviews
-        </a>
-        <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/restaurants"} className="nav-link">
-              Restaurants
-            </Link>
-          </li>
-          <li className="nav-item" >
-            { user ? (
-              <a onClick={logout} className="nav-link" style={{cursor:'pointer'}}>
-                Logout {user.name}
-              </a>
-            ) : (            
-            <Link to={"/login"} className="nav-link">
-              Login
-            </Link>
-            )}
-          </li>
-        </div>
-      </nav>
