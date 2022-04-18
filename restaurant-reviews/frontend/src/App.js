304dc89239eae4af40d66c5d9aaa5fa441301f3e
Mukhtar <mukhtartanimugarba@gmail.com>
2022-04-18 07:41:58 +0100

    Update file

--- restaurant-reviews/frontend/src/App.js
+++ restaurant-reviews/frontend/src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import AddReview from "./components/add-review";
+import Restaurant from "./components/restaurants";
+import RestaurantsList from "./components/restaurants-list";
+import Login from "./components/login";
+
 
 function App() {
   
